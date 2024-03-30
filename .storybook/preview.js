11debd6952d9fc3b02feb678081c3c95576066cb
Oguzhan Ozcan <ouzmetu@gmail.com>
2024-03-30 16:19:50 +0300

    connected::zeplin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    zeplinLink: "https://zpl.io/1MJKY05",
     controls: {
       matchers: {
         color: /(background|color)$/i,
